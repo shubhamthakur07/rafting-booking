824edb2e62c157e8fb0dcca1844b8e6409ed69bd
shubham thakur <shubhamthakurst07@gmail.com>
2026-03-25 10:12:56 +0530

    build(config): add https and hmr configuration for dev server

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,10 @@ export default defineConfig({
             },
         }),
     ],
+     server: {
+        https: true,
+        hmr: {
+            host: 'efficient-miracle-production.up.railway.app',
+        },
+    },
 });
